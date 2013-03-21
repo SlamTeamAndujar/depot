032457b7d4024ede94004110e26e376a80a51558
Pierranne Lenck <lenck.pa@gmail.com>
2013-03-21 16:53:44 +0100

    nouveau fichier

--- vues/v_medicaments.php
+++ vues/v_medicaments.php
@@ -1,45 +1,64 @@
-<div>
+<div id="contenu">
     <h2>Pharmacopee</h2>
     
         
     <?php
-       foreach ($lesMedicaments as $unMedicament) {
-			$depotLegal = $unMedicament['MED_DEPOTLEGAL'];
-			$nomCommercial = $unMedicament['MED_NOMCOMMERCIAL'];
-                        $famille = $unMedicament['FAM_LIBELLE'];
-                        $composition = $unMedicament['MED_COMPOSITION'];
-                        $contreIndic = $unMedicament['MED_CONTREINDIC'];
-                        $prixEchantillon = $unMedicament['MED_PRIXECHANTILLON'];
-       }
+    
+       
+			$depotLegal = $lesMedicaments[$num]['MED_DEPOTLEGAL'];
+			$nomCommercial = $lesMedicaments[$num]['MED_NOMCOMMERCIAL'];
+                        $famille = $lesMedicaments[$num]['FAM_LIBELLE'];
+                        $composition = $lesMedicaments[$num]['MED_COMPOSITION'];
+                        $contreIndic = $lesMedicaments[$num]['MED_CONTREINDIC'];
+                        $prixEchantillon = $lesMedicaments[$num]['MED_PRIXECHANTILLON'];
+       
      ?>                   
         <table border="1">
         <tr>
-            <td>Dépot légal</td>
+            <th bgcolor="#77AADD">Dépot légal</th>
             <td><?php echo $depotLegal; ?></td>
         </tr>
         <tr>
-            <td>Nom Commercial</td>
+            <th bgcolor="#77AADD">Nom Commercial</th>
             <td><?php echo $nomCommercial; ?></td>
         </tr>
         <tr>
-            <td>Famille</td>
+            <th bgcolor="#77AADD">Famille</th>
             <td><?php echo $famille; ?></td>
         </tr>
         <tr>
-            <td>Composition</td>
+            <th bgcolor="#77AADD">Composition</th>
             <td><?php echo $composition; ?></td>
         </tr>
         <tr>
-            <td>Contre-indication</td>
+            <th bgcolor="#77AADD">Contre-indication</th>
             <td><?php echo $contreIndic; ?></td>
         </tr>
         <tr>
-            <td>prix échantillon</td>
+            <th bgcolor="#77AADD">prix échantillon</th>
             <td><?php echo $prixEchantillon; ?></td>
         </tr>
         </table>
-    <a href=""><input type ="button" value ="Précédent"></a>
-    <a href=""><input type ="button" value ="Suivant"></a>
+    <?php 
+        if ($num == 0){
+    ?>
+            
+            <a href="index.php?uc=consulterMedicaments&action=voirMedicaments&num=<?php echo $num + 1; ?>"><input type ="button" value ="Suivant"></a>
+    <?php
+        }
+        elseif ($num ==27){
+    ?>
+            <a href="index.php?uc=consulterMedicaments&action=voirMedicaments&num=<?php echo $num - 1; ?>"><input type ="button" value ="Précédent"></a>
+            
+    <?php
+        }
+        else{
+    ?>
+            <a href="index.php?uc=consulterMedicaments&action=voirMedicaments&num=<?php echo $num - 1; ?>"><input type ="button" value ="Précédent"></a>
+            <a href="index.php?uc=consulterMedicaments&action=voirMedicaments&num=<?php echo $num + 1; ?>"><input type ="button" value ="Suivant"></a>
+    <?php        
+        }    
+    ?>
     
     
    
